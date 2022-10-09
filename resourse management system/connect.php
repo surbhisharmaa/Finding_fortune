d31d568f667dcad37465ae630e8daa517a1f6a8f
Surbhi Sharma <91070419+surbhisharmaa@users.noreply.github.com>
2022-10-09 15:57:04 +0530

    Add files via upload

---
+++ resourse management system/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $yourname = $_POST['yourname'];
+    $yourname = $_POST['mobile no.'];
+    $yourname = $_POST['select'];
+    $yourname = $_POST['address'];
+
+    //database connection
+?>
